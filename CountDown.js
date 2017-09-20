c50f673134866c34bac159033ac22a357648a1fb
rwStarnight <29952469+rwStarnight@users.noreply.github.com>
2017-09-20 10:08:34 -0400

    little change
    parent & child

--- CountDown.js
+++ CountDown.js
@@ -29,10 +29,20 @@ class CountDown extends Component {
         <View>{this.renderResult()}</View>
     )
   }
+  add = (time) => {
+    this.setState({
+      count: this.state.count + time
+    })
+  }
+  timeup = () =>{
+    alert('时间到！');
+    this.props.timeupParent && this.props.timeupParent('子组件传回的参数' + this.state.test);
+  }
   componentDidMount() {
     this.timer = setInterval(() => {
       const { count } = this.state;
       if (count === 0) {
+        this.timeup();
         return clearInterval(this.timer);
       }
       this.setState({
@@ -46,10 +56,20 @@ class CountDown extends Component {
 }
 
 class App extends Component {
+  timeupParent = (param) => {
+    alert('抓紧时间')
+  }
+  state = {
+    arr: [1,2,3]
+  }
   render() {
     return (
       <View>
-        <CountDown />
+      {
+        this.state.arr.map(i => {
+          return<CountDown key{i} timeupParent={(child) => this.timeupParent('父组件用于区分的参数' + i,child)} />
+        })
+      }
       </View>
     )
   }
